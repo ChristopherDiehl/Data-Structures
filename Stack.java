a7a7ad2f5493bbf62b7b962baf40f68ac0d77d21
ChristopherDiehl <csd92@scarletmail.rutgers.edu>
2015-10-08 12:22:45 -0400

    first-commit

---
+++ Stack.java
@@ -0,0 +1,44 @@
+public class Stack {
+	private Node firstNode;
+	private Node lastNode;
+	private int numberOfDigits = 0;
+	public Stack () {
+
+	}
+	public void push(int data) {
+		if (firstNode == null) {
+			Node firstNode = new Node(null, data, null);
+			lastNode = firstNode;
+			numberOfDigits ++;
+		}
+		else {
+			Node newNode = new Node(lastNode, digit, null);
+			lastNode.next = newNode;
+			lastNode = newNode;
+			numberOfDigits ++;
+		}
+		this.data = data;
+	}
+	public int pop() {
+		if(firstNode == null) {
+			return -1;
+		}
+		else {
+			int returnVal = lastNode.data;
+			lastNode = lastNode.previous;
+			return returnVal;
+		}
+	}
+	//doubly linked list
+	private class Node  {
+		private int date;
+		private Node next;
+		private Node previous;
+
+		private Node (Node previousNode, int aData, Node nextNode) {
+			previous = previousNode;
+			data = aData;
+			next= nextNode;
+		}
+	}
+}
