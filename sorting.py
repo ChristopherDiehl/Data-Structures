a125c7ae123a1cc7905d41632bbe8da618e63b4f
ChristopherDiehl <csd92@scarletmail.rutgers.edu>
2015-11-27 11:18:33 -0500

    Adding some python sorting algorithms... slowly, but surely;

--- sorting.py
+++ sorting.py
@@ -1,18 +1,29 @@
-import random
-#just create an unsorted array to sort 
-array = [random.randint(0,1000) for r in xrange (1000)]
-val = array[0]
-for index in range (1,len(array)):
-	indexVal = array[index]
-	smallestIntVal = index
-	for secondInd in range (index+1, len(array))
-		if array[secondInd] < array[index]:
-			smallestIntVal = secondInd
-			index = secondInd
+class Sorting:
+	"""A Class to learn Python and some sorting"""
+	import random
+	#just create an unsorted array to sort 
+	
+	def __init__():
+		array = [random.randint(0,1000) for r in xrange (1000)]
+		selectionSort(array)
 
-	array[index] = array[smallestIntVal]
-	array[smallestIntVal] = indexVal
-print array
+	def selectionSort(array):
+		val = array[0]
+		for index in range (1,len(array)):
+			indexVal = array[index]
+			smallestIntVal = index
+			for secondInd in range (index+1, len(array)):
+				if array[secondInd] < array[index]:
+					smallestIntVal = secondInd
+					index = secondInd
+
+			array[index] = array[smallestIntVal]
+			array[smallestIntVal] = indexVal
+	
+		print array
+	
+
+
 
 
 
