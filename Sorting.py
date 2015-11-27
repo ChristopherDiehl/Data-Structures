1c56bc8c2a1aa5246e3c77320cb74ff4be306120
ChristopherDiehl <csd92@scarletmail.rutgers.edu>
2015-11-27 18:45:15 -0500

    Changed name and testing selectionSort

--- Sorting.py
+++ Sorting.py
@@ -1,13 +1,10 @@
+import random
+
+
 class Sorting:
 	"""A Class to learn Python and some sorting"""
-	import random
-	#just create an unsorted array to sort 
-	
-	def __init__():
-		array = [random.randint(0,1000) for r in xrange (1000)]
-		selectionSort(array)
 
-	def selectionSort(array):
+	def selectionSort(self, array):
 		val = array[0]
 		for index in range (1,len(array)):
 			indexVal = array[index]
@@ -22,7 +19,8 @@ class Sorting:
 	
 		print array
 	
-
+array = [random.randint(0,1000) for r in xrange (1000)]
+sor.selectionSort(array);
 
 
 
