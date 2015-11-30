91ec2f412d50a93c7ea3b64b482007b23f0040cd
ChristopherDiehl <csd92@scarletmail.rutgers.edu>
2015-11-30 14:09:05 -0500

    Added bubble sort

--- Sorting.py
+++ Sorting.py
@@ -18,9 +18,19 @@ class Sorting:
 			array[smallestIntVal] = indexVal
 	
 		print array
-	
+
+	def bubbleSort(self, array) :
+		for index in range (1, len(array) -1):
+			if array[index-1] > array [index]
+				temp = array [index]
+				array[index] = array[index-1]
+				array[index-1] = temp
+		print array
+
+
 array = [random.randint(0,1000) for r in xrange (1000)]
-sor.selectionSort(array);
+sor.selectionSort(array)
+sor.bubbleSort(array)
 
 
 
