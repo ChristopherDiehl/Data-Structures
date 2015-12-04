5182b9ce472ca09870ad0e41afa9e60c0426541e
ChristopherDiehl <csd92@scarletmail.rutgers.edu>
2015-12-03 19:32:12 -0500

    digitTest.py is done

--- digitTest.py
+++ digitTest.py
@@ -1,11 +1,11 @@
 """A python program to print words for individual digits without if statement"""
 numList = ['zero','one','two','three','four','five','six','seven','eight','nine']
 def printTheWords(n):
-    if not is_integer(n):
-		print "Not Valid Input"
-	else:
+	try:
 		print numList[n%10]
 		if n >=10 :
 			printTheWords(n/10)
-
-
+	except TypeError:
+		print "Invalid Input"
+printTheWords(50)
+printTheWords
