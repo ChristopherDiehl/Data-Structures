74998578153d6947ac046aed2adb045279fe8f3d
ChristopherDiehl <csd92@scarletmail.rutgers.edu>
2016-06-08 15:01:23 -0400

    added insertion sort

--- bubblesort.c
+++ bubblesort.c
@@ -2,12 +2,15 @@
 #include <stdlib.h>
 
 void bubbleSort(int arr [], int arrLen);
+void insertionSort(int arr[], int arrLen);
 
 int main() {
 
-	int array [7] = {5,1,3,7,10,25,11};
+	int array [7] = {5,1,9,7,10,25,11};
 
-	bubbleSort(array,7);
+	//bubbleSort(array,7);
+	insertionSort(array,7);
+
 }
 
 void bubbleSort(int arr [], int arrLen)
@@ -29,6 +32,31 @@ void bubbleSort(int arr [], int arrLen)
 	}
 
 	for(i =0; i <arrLen; i++)
+	{
+		printf("%d,",arr[i]);
+	}
+}
+
+void insertionSort(int arr[], int arrLen)
+{
+	int i,key,j;
+
+	for(i = 1; i < arrLen; i++)
+	{
+
+		key = arr[i];
+		j = i -1;
+		while(j >= 0 && arr[j] > key)
+		{
+
+			arr[j+1] = arr[j];
+			j--;
+		}
+		arr[j +1] = key;
+	}
+	
+
+	for(i = 0; i < arrLen; i++)
 	{
 		printf("%d,",arr[i]);
 	}
